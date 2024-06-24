889e9e7136ae906520c7325287b5f319e151e96f
DALT-DESGIT04 <jpizarra@dincors.com>
2024-06-24 01:38:22 -0500

    comments

--- config/db.py
+++ config/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
+#configuration to connect to database
 engine = create_engine("mysql+pymysql://root:password@localhost:3306/storedb")
 
 meta = MetaData()
--- schemas/user.py
+++ schemas/user.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 
+#model that represents the database columns that we will use for queries
 class User(BaseModel):
     id: Optional[int] = None
     name: str
